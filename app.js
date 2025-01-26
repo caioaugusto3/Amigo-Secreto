04dc13a9058601566c2f1f6a2fcbf7101f05d389
Caio Augusto <caioaugusto3@gmail.com>
2025-01-26 14:52:56 -0300

    Capturar o valor do campo de entrada

---
+++ app.js
@@ -0,0 +1,7 @@
+let listaAmigos = [];
+
+// Capturar o valor do campo de entrada para obter o texto inserido pelo usuário
+function adicionarAmigo() {
+    let amigos = document.getElementById('amigo').value;
+    console.log(amigos);  
+}
