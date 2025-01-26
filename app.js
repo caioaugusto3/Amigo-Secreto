5be4d545d830405f73edbc51913d92ec0f8a3b4c
Caio Augusto <caioaugusto3@gmail.com>
2025-01-26 15:14:16 -0300

    valida entrada campo vazio e Exibi um alerta caso esteja vazio

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 let listaAmigos = [];
 
-// Capturar o valor do campo de entrada para obter o texto inserido pelo usuário
+// Capturar o valor do campo de entrada para obter o texto inserido pelo usuário.
 function adicionarAmigo() {
     let amigos = document.getElementById('amigo').value;
-    console.log(amigos);  
+    console.log(amigos);
+// Valida entrada para que o campo não esteja vazio. Exibi um alerta caso esteja vazio.    
+    if (amigos === '') {
+        alert('Por favor, insira um nome.');
+    } 
 }
